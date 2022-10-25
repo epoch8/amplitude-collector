bc08df295368326e66af926ef827356bdd4d834b
Eldar <eldar.shkh@gmail.com>
2022-10-25 13:55:45 +0300

    added verification that the data appeared in kafka

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,12 @@
+from uuid import uuid4
+
 import pytest
 from starlette.testclient import TestClient
+from kafka.admin import KafkaAdminClient, ConfigResource
+from kafka import KafkaConsumer
 
 from src.app import app
+from src.config import KAFKA_TOPIC, KAFKA_DSN
 
 
 @pytest.fixture(scope="session")
@@ -10,3 +15,31 @@ def client():
     yield client
 
 
+@pytest.fixture(scope="session", autouse=True)
+def admin_client():
+    admin_client = KafkaAdminClient(
+        bootstrap_servers=KAFKA_DSN,
+        client_id='test'
+    )
+    topic_list = []
+    topic_list.append(ConfigResource(
+        resource_type='TOPIC', name=KAFKA_TOPIC, configs={"retention.ms": "60000"})
+    )
+    admin_client.alter_configs(topic_list)
+    yield
+
+
+@pytest.fixture(scope="function")
+def kafka_consumer():
+    consumer = KafkaConsumer(KAFKA_TOPIC, bootstrap_servers=KAFKA_DSN, auto_offset_reset='earliest')
+    yield consumer
+
+
+@pytest.fixture(scope="function")
+def generate_test_json():
+    return {"id": str(uuid4()), "key": "test", "key2": 1}
+
+
+@pytest.fixture(scope="function")
+def generate_test_form():
+    return {"id": str(uuid4()), "key": "test", "key2": "1"}
--- tests/test_collect.py
+++ tests/test_collect.py
@@ -1,18 +1,32 @@
+import json
+
+import pytest
 from starlette.testclient import TestClient
 
 
-def test_collect_json(client: TestClient):
+def get_data_from_kafka(data: dict, kafka_consumer) -> bool:
+    data_in_kafka = []
+    msg_pack = kafka_consumer.poll(timeout_ms=1000)
+    for tp, messages in msg_pack.items():
+        for msg in messages:
+            data_in_kafka.append(json.loads(msg.value))
+    for msgs in data_in_kafka:
+        if msgs.get("id") == data["id"]:
+            return msgs
+
+
+def test_collect_json(client: TestClient, kafka_consumer, generate_test_json):
     client.headers = {"content-type": "application/json"}
-    data = {"key1": 1, "key2": "2", "key3": "test"}
-    response = client.post("/collect", json=data)
+    response = client.post("/collect", json=generate_test_json)
     assert response.status_code == 200
+    assert get_data_from_kafka(generate_test_json, kafka_consumer) == generate_test_json
 
 
-def test_collect_form_data(client: TestClient):
+def test_collect_form_data(client: TestClient, kafka_consumer, generate_test_form):
     client.headers = {"content-type": "application/x-www-form-urlencoded"}
-    data = {"key1": 1, "key2": "2", "key3": "test"}
-    response = client.post("/collect", data=data)
+    response = client.post("/collect", data=generate_test_form)
     assert response.status_code == 200
+    assert get_data_from_kafka(generate_test_form, kafka_consumer) == generate_test_form
 
 
 def test_collect_unexpected_content_type(client: TestClient):
