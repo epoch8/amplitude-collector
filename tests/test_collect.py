990b4cc3b59a2520ef50157f5e41acd7f9ff8682
Andrey Tatarinov <a@tatarinov.co>
2022-10-30 21:29:54 +0400

    *

--- tests/test_collect.py
+++ tests/test_collect.py
@@ -14,18 +14,22 @@ def get_data_from_kafka(data: dict, kafka_consumer):
             return msgs
 
 
+def assert_kafka_msg_eq(kafka_msg, msg):
+    assert kafka_msg["e"] == json.loads(msg["e"])
+
+
 def test_collect_json(client: TestClient, kafka_consumer, generate_test_json):
     client.headers = {"content-type": "application/json"}  # type: ignore
     response = client.post("/collect", json=generate_test_json)
     assert response.status_code == 200
-    assert get_data_from_kafka(generate_test_json, kafka_consumer) == generate_test_json
+    assert_kafka_msg_eq(get_data_from_kafka(generate_test_json, kafka_consumer), generate_test_json)
 
 
 def test_collect_form_data(client: TestClient, kafka_consumer, generate_test_form):
     client.headers = {"content-type": "application/x-www-form-urlencoded"}  # type: ignore
     response = client.post("/collect", data=generate_test_form)
     assert response.status_code == 200
-    assert get_data_from_kafka(generate_test_form, kafka_consumer) == generate_test_form
+    assert_kafka_msg_eq(get_data_from_kafka(generate_test_form, kafka_consumer), generate_test_form)
 
 
 def test_collect_unexpected_content_type(client: TestClient):
