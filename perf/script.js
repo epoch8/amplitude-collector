7328fb11a1e5e2c733247db226b3f1af7f429d2d
Andrey Tatarinov <a@tatarinov.co>
2023-10-09 01:19:39 +0400

    perf: add check, remove sleep

--- perf/script.js
+++ perf/script.js
@@ -1,6 +1,5 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
-import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { sleep, check } from 'k6';
 
 import { oneMessagePayload, threeMessagePayload, hundredMessagePayload } from './payloads.js';
 
@@ -10,10 +9,12 @@ export const options = {
 };
 
 export default function () {
-    http.post(
+    const res = http.post(
         `http://${__ENV.TARGET_HOST}/collect`,
         hundredMessagePayload,
         { headers: { 'Content-Type': 'application/json' } }
     );
-    sleep(1);
+    check(res, {
+        'is status 200': (r) => r.status === 200,
+    });
 }
